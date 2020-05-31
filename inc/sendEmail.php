75fa20a6440aa51861783db398727b9b99ebef86
Sukhjeevan Bansal <princebansal2209@gmail.com>
2020-05-31 17:00:22 +0530

    Added headers php

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,5 +1,9 @@
 ﻿<?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 // Replace this with your own email address
 $siteOwnersEmail = 'princebansal2209@gmail.com';
 
